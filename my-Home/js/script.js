4e52ddcbd29dd96e6f1c6d9dea5c850e57157d21
Samuel Santana <35262710+turan-98@users.noreply.github.com>
2019-05-03 18:12:44 -0300

    Update js
    adicionando novas funcionalidades

--- my-Home/js/script.js
+++ my-Home/js/script.js
@@ -1,14 +1,23 @@
-
+/*
+const $fas = document.querySelector('.fab')
+const $close = document.querySelector('#close_btn')
+const $sidebar = document.querySelector('.sidebar')
+$fas.addEventListener('click',function(){
+    $sidebar.classList.add('active')
+})
+$close.addEventListener('click',function(){
+    $sidebar.classList.remove('active')
+})
+*/
 function main(){
   $('.container').hide();
   $('.container').fadeIn(1000);
   $('.fas').on('click',function(){
     $('.sidebar').toggleClass('active');
   });
+  $('#close_btn').on('click', function(){
+    $('.sidebar').removeClass('active');
+  })
 }
 $(document).ready(main);
   
- /*button = document.querySelector('.fab').addEventListener("click", function(event){
-  document.querySelector('.sidebar').classList.toggle('active');
-},false)
-*/
