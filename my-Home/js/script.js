ea8a9b8ca46ca02365266760b14601319e280a03
Samuel Santana <35262710+turan-98@users.noreply.github.com>
2019-05-06 17:25:44 -0300

    Update script.js

--- my-Home/js/script.js
+++ my-Home/js/script.js
@@ -2,22 +2,11 @@
 const $fas = document.querySelector('.open_menu')
 const $close = document.querySelector('.close_menu')
 const $sidebar = document.querySelector('.sidebar')
+
 $fas.addEventListener('click',function(){
     $sidebar.classList.add('active')
 })
 $close.addEventListener('click',function(){
     $sidebar.classList.remove('active')
 })
-/*
-function main(){
-  $('.container').hide();
-  $('.container').fadeIn(1000);
-  $('.fas').on('click',function(){
-    $('.sidebar').toggleClass('active');
-  });
-  $('#close_btn').on('click', function(){
-    $('.sidebar').removeClass('active');
-  })
-}
-$(document).ready(main);
-*/
+
