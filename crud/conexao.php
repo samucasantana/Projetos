424eb8b62fa04f3cd4faca6d2570995fdc15b84f
Samuel Santana <35262710+turan-98@users.noreply.github.com>
2019-07-30 19:31:28 -0300

    Create conexao.php

---
+++ crud/conexao.php
@@ -0,0 +1,14 @@
+
+	<?php 
+	define('MYSQL_HOST','localhost');
+	define('MYSQL_USER','root');
+	define('MYSQL_PASSWORD','');
+	define('MYSQL_DB_NAME','bancopoo');
+	try{
+		$PDO = new PDO('mysql:host='. MYSQL_HOST . ';dbname='. MYSQL_DB_NAME,MYSQL_USER,MYSQL_PASSWORD);
+		$PDO->exec("set names utf8");
+	}
+	catch(PDOException $e){
+		echo " Erro ao conectar com o MySQL: ". $e->getMessage();
+	}
+	?>
